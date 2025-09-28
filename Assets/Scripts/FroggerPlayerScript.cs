212e6d5542e57aaf226c2d5f72a3f961850d022c
Zhamanta Chavez <zhamantach04@gmail.com>
2025-09-27 19:48:42 -0500

    Player Movement/Animation

--- Assets/Scripts/FroggerPlayerScript.cs
+++ Assets/Scripts/FroggerPlayerScript.cs
@@ -1,16 +1,69 @@
+using System.Collections;
 using UnityEngine;
 
 public class FroggerPlayerScript : MonoBehaviour
 {
+    Animator anim;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerUpdate();
     }
+
+    void PlayerUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            PlayerMove(Vector3.up);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+            PlayerMove(Vector3.down);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+            PlayerMove(Vector3.left);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 270f);
+            PlayerMove(Vector3.right);
+        }
+    }
+
+    void PlayerMove(Vector3 direction)
+    {
+        Vector3 destination = transform.position + direction; 
+        StartCoroutine(SmoothMove(destination));
+    }
+
+    IEnumerator SmoothMove(Vector3 destination)
+    {
+        Vector3 startPos = transform.position;
+        anim.SetTrigger("Hop");
+
+        float elapsed = 0f;
+        float duration = .125f;
+
+        while (elapsed < duration)
+        {
+            float t = elapsed / duration;
+            transform.position = Vector3.Lerp(startPos, destination, t);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = destination;
+    }
+
 }
